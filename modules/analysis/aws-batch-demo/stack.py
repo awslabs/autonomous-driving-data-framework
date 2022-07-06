a664e3b42e8d4828933ae1119b36009d103275c7
Kevin Soucy <soucyk@amazon.de>
2022-07-06 11:16:48 +0200

    add example dag

---
+++ modules/analysis/aws-batch-demo/stack.py
@@ -0,0 +1,180 @@
+#  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License").
+#    You may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import logging
+from typing import Any, Dict, cast
+
+import aws_cdk.aws_ec2 as ec2
+import aws_cdk.aws_ecr as ecr
+import aws_cdk.aws_iam as iam
+import cdk_nag
+from aws_cdk import Aspects, Duration, RemovalPolicy, Stack, Tags
+from aws_cdk import aws_dynamodb as dynamo
+from cdk_nag import NagSuppressions
+from constructs import Construct, IConstruct
+
+_logger: logging.Logger = logging.getLogger(__name__)
+
+
+class AwsBatchPipeline(Stack):  # type: ignore
+    def __init__(
+        self,
+        scope: Construct,
+        id: str,
+        *,
+        config: Dict[str, Any],
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(
+            scope,
+            id,
+            **kwargs,
+        )
+
+        for k, v in config.items():
+            setattr(self, k, v)
+
+        Tags.of(scope=cast(IConstruct, self)).add(
+            key="Deployment", value=f"aws",
+        )
+
+        dep_mod = f"addf-{self.deployment_name}-{self.module_name}"
+
+        self.vpc = ec2.Vpc.from_lookup(
+            self,
+            "VPC",
+            vpc_id=self.vpc_id,
+        )
+
+        self.repository_name = dep_mod
+        ecr.Repository(self, id=self.repository_name, repository_name=self.repository_name)
+
+        # DYNAMODB TRACKING TABLE
+        self.tracking_table_name = f"{dep_mod}-drive-tracking"
+        tracking_partition_key = "pk"  # batch_id or drive_id
+        tracking_sort_key = "sk"  # batch_id / array_index_id   or drive_id / file_part
+
+        dynamo.Table(
+            self,
+            self.tracking_table_name,
+            table_name=self.tracking_table_name,
+            partition_key=dynamo.Attribute(name=tracking_partition_key, type=dynamo.AttributeType.STRING),
+            sort_key=dynamo.Attribute(name=tracking_sort_key, type=dynamo.AttributeType.STRING),
+            billing_mode=dynamo.BillingMode.PAY_PER_REQUEST,
+            removal_policy=RemovalPolicy.DESTROY,
+            point_in_time_recovery=True,
+            stream=dynamo.StreamViewType.NEW_AND_OLD_IMAGES,
+        )
+
+        # tracking_table.add_global_secondary_index(
+        #     index_name="batch_file_lookup",
+        #     partition_key=dynamo.Attribute(name="batch_id", type=dynamo.AttributeType.STRING),
+        #     sort_key=dynamo.Attribute(name="array_index_id", type=dynamo.AttributeType.NUMBER)
+        # )
+
+        # Create Dag IAM Role and policy
+        policy_statements = [
+            iam.PolicyStatement(
+                actions=["dynamodb:*"],
+                effect=iam.Effect.ALLOW,
+                resources=[f"arn:aws:dynamodb:{self.region}:{self.account}:table/{dep_mod}*"],
+            ),
+            iam.PolicyStatement(
+                actions=["ecr:*"],
+                effect=iam.Effect.ALLOW,
+                resources=[f"arn:aws:ecr:{self.region}:{self.account}:repository/{dep_mod}*"],
+            ),
+            iam.PolicyStatement(
+                actions=["s3:*"],
+                effect=iam.Effect.ALLOW,
+                resources=["*"],
+            ),
+            iam.PolicyStatement(
+                actions=[
+                    "batch:UntagResource",
+                    "batch:DeregisterJobDefinition",
+                    "batch:TerminateJob",
+                    "batch:CancelJob",
+                    "batch:SubmitJob",
+                    "batch:RegisterJobDefinition",
+                    "batch:TagResource",
+                ],
+                effect=iam.Effect.ALLOW,
+                resources=[
+                    f"arn:aws:batch:{self.region}:{self.account}:job-queue/addf*",
+                    f"arn:aws:batch:{self.region}:{self.account}:job-definition/*",
+                    f"arn:aws:batch:{self.region}:{self.account}:job/*",
+                ],
+            ),
+            iam.PolicyStatement(
+                actions=[
+                    "iam:PassRole",
+                ],
+                effect=iam.Effect.ALLOW,
+                resources=[
+                    f"arn:aws:iam::{self.account}:role/addf*",
+                ],
+            ),
+            iam.PolicyStatement(
+                actions=[
+                    "batch:Describe*",
+                    "batch:List*",
+                ],
+                effect=iam.Effect.ALLOW,
+                resources=[
+                    "*",
+                ],
+            ),
+        ]
+        dag_document = iam.PolicyDocument(statements=policy_statements)
+
+        batch_role_name = f"{dep_mod}-dag-role"
+
+        self.dag_role = iam.Role(
+            self,
+            f"dag-role-{dep_mod}",
+            assumed_by=iam.CompositePrincipal(
+                iam.ArnPrincipal(self.mwaa_exec_role),
+                iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
+            ),
+            inline_policies={"DagPolicyDocument": dag_document},
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AmazonECSTaskExecutionRolePolicy"),
+                iam.ManagedPolicy.from_managed_policy_arn(
+                    self, id="fullaccess", managed_policy_arn=self.full_access_policy
+                ),
+            ],
+            role_name=batch_role_name,
+            max_session_duration=Duration.hours(12),
+            path="/",
+        )
+
+        Aspects.of(self).add(cdk_nag.AwsSolutionsChecks())
+
+        NagSuppressions.add_stack_suppressions(
+            self,
+            apply_to_nested_stacks=True,
+            suppressions=[
+                {
+                    "id": "AwsSolutions-IAM4",
+                    "reason": "Managed Policies are for service account roles only",
+                    "applies_to": "*",
+                },
+                {
+                    "id": "AwsSolutions-IAM5",
+                    "reason": "Resource access restriced to ADDF resources",
+                    "applies_to": "*",
+                },
+            ],
+        )
