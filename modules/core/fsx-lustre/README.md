ee62a6a33148ed0bbfe4b96b5231e4886c650967
Ramon Bautista <bautramo@amazon.com>
2023-01-19 17:10:07 -0500

    FSx on Lustre module refactor/cleanup

---
+++ modules/core/fsx-lustre/README.md
@@ -0,0 +1,47 @@
+## Introduction
+new-module
+
+### TODO:-
+
+* Create Stack for fsx lustre creation 
+* Create stack for fsx s3 mount
+* Create Stack for launch template
+* Update AWS Batch compute with launch template
+* Create Docker images for 1 + 3 + 2 use case workflow container
+* Automate Deployment script with docker build and push
+* Create AWS Batch Job Definition with docker and fsx host mount 
+* Manually upload sample datas to raw buckets
+* Create Airflow Dag with workflow code
+
+
+## Description
+
+A short description of the module or project.
+
+## Architecture
+
+
+
+## Deployment
+
+
+## Inputs/Outputs
+
+
+### Input Parameters
+
+
+#### Required
+
+
+#### Optional
+
+
+#### Input Example
+
+
+### Module Metadata Outputs
+
+
+
+#### Output Example
---
+++ modules/core/fsx-lustre/app.py
@@ -0,0 +1,57 @@
+import json
+import os
+
+from aws_cdk import App, CfnOutput, Environment
+from stack import FsxFileSystem
+
+deployment_name = os.getenv("ADDF_DEPLOYMENT_NAME", "")
+module_name = os.getenv("ADDF_MODULE_NAME", "")
+
+
+def _param(name: str) -> str:
+    return f"ADDF_PARAMETER_{name}"
+
+vpc_id = os.getenv(_param("VPC_ID"))  # required
+private_subnet_ids = json.loads(os.getenv(_param("PRIVATE_SUBNET_IDS")))  # required
+raw_bucket_name = os.getenv(_param("RAW_BUCKET_NAME"))
+interm_bucket_name = os.getenv(_param("INTERMEDIATE_BUCKET_NAME"))
+curated_bucket_name = os.getenv(_param("CURATED_BUCKET_NAME"))
+
+
+if not vpc_id:
+    raise Exception("missing input parameter vpc-id")
+
+if not private_subnet_ids:
+    raise Exception("missing input parameter private-subnet-ids")
+
+app = App()
+
+stack = FsxFileSystem(
+    scope=app,
+    id=f"addf-{deployment_name}-{module_name}",
+    deployment_name=deployment_name,
+    module_name=module_name,
+    vpc_id=vpc_id,
+    private_subnet_ids=private_subnet_ids,
+    raw_bucket_name=raw_bucket_name,
+    interm_bucket_name=interm_bucket_name,
+    curated_bucket_name=curated_bucket_name,
+    env=Environment(
+        account=os.environ["CDK_DEFAULT_ACCOUNT"],
+        region=os.environ["CDK_DEFAULT_REGION"]
+    )
+)
+
+CfnOutput(
+    scope=stack,
+    id="metadata",
+    value=stack.to_json_string(
+        {
+            "FsxLustreFileSystemId": stack.fsx_filesystem.ref,
+            "FsxLustreAttrDnsName": stack.fsx_filesystem.attr_dns_name,
+            "FsxLustreAttrMountName": stack.fsx_filesystem.attr_lustre_mount_name,
+        }
+    ),
+)
+
+app.synth(force=True)
