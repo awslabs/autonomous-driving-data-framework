63fe3d4b07cddfb8d05da71e434e09cf031aa3ad
Derek Graeber <dgraeber@amazon.com>
2023-01-24 11:38:08 -0500

    fsx eks support

---
+++ modules/integration/fsx-lustre-on-eks/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md") as fp:
+    long_description = fp.read()
+
+setuptools.setup(
+    version="0.1.0",
+    description="A module thatintegrates an FSx service with an EKS cluster",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="dgraeber@amazon.com",
+    install_requires=[
+        "aws-cdk-lib==2.49.1",
+    ],
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Typing :: Typed",
+    ],
+)
