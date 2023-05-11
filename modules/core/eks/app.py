01774c38d08aa3812b232d6aacd7eb40e71f1c30
srinivasreddych <srinivasreddych@outlook.com>
2023-05-11 08:43:13 -0500

    checking for existence of replications metadata

--- modules/core/eks/app.py
+++ modules/core/eks/app.py
@@ -20,7 +20,9 @@ if os.getenv("ADDF_PARAMETER_CODEBUILD_SG_ID"):
     codebuild_sg_id = json.loads(os.getenv("ADDF_PARAMETER_CODEBUILD_SG_ID"))[0]
 
 # TODO: rosty use the below to grab the images metadata
-replicated_ecr_images_metadata = json.loads(os.getenv("ADDF_PARAMETER_REPLICATED_ECR_IMAGES_METADATA"))
+replicated_ecr_images_metadata = (
+    json.loads(os.getenv("ADDF_PARAMETER_REPLICATED_ECR_IMAGES_METADATA")) if os.getenv("ADDF_PARAMETER_REPLICATED_ECR_IMAGES_METADATA") else None
+)
 
 if not vpc_id:
     raise ValueError("missing input parameter vpc-id")
