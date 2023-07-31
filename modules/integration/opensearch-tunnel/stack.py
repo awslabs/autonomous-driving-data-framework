41ad91c3732c64a93237358dccbf3a937f6c1ef7
Derek Graeber <dgraeber@amazon.com>
2023-07-31 16:10:21 -0400

    correcting device_name

--- modules/integration/opensearch-tunnel/stack.py
+++ modules/integration/opensearch-tunnel/stack.py
@@ -124,7 +124,7 @@ class TunnelStack(Stack):
             vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS),
             role=os_tunnel_role,
             block_devices=[ec2.BlockDevice(
-                            device_name="EC2TunnelHost",
+                            device_name="/dev/xvda",
                             volume=ec2.BlockDeviceVolume.ebs(10,
                                 encrypted=True
                             )
