dc1081e0e9963b6a860296e86ddcd43f0f99198b
Derek Graeber <dgraeber@amazon.com>
2023-08-22 14:17:06 -0400

    sycing ros-to-png

--- modules/sensor-extraction/ros-to-png/src/main.py
+++ modules/sensor-extraction/ros-to-png/src/main.py
@@ -1,16 +1,5 @@
-#  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-#    Licensed under the Apache License, Version 2.0 (the "License").
-#    You may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
-#
-#        http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
 
 import argparse
 import concurrent
@@ -191,7 +180,6 @@ def extract_images(bag_path, topic, resized_width, resized_height, encoding, ima
             all_files += bag_obj.files
             logger.info(f"Images extracted from topic: {topic} with encoding {encoding}")
 
-        # video_file = VideoFromBag(topic, images_path)
     except rospy.ROSInterruptException:
         pass
     return all_files
@@ -222,7 +210,7 @@ def main(table_name, index, batch_id, bag_path, images_path, topics, encoding, t
     logger.info("Item Pulled: %s", item)
 
     if not item:
-        raise Exception(f"pk: {batch_id} sk: {index} not existing in table: {table_name}")
+        raise ValueError(f"pk: {batch_id} sk: {index} not existing in table: {table_name}")
 
     drive_id = item["drive_id"]
     file_id = item["file_id"]
