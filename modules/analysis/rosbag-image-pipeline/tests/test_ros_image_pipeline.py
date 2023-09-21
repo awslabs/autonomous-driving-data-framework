4d420b914447f5757f335659a5bbbc72d7fe6e87
Derek Graeber <dgraeber@amazon.com>
2023-09-21 16:11:08 -0400

    updating tests

--- modules/analysis/rosbag-image-pipeline/tests/test_ros_image_pipeline.py
+++ modules/analysis/rosbag-image-pipeline/tests/test_ros_image_pipeline.py
@@ -1,7 +1,7 @@
 from image_dags.ros_image_pipeline import *
 
 
-def test_validate_config():
+def test_validate_config(aws_credentials):
     input = {
         "drive2": {
             "bucket": "addf-ros-image-demo-raw-bucket-d2be7d29",
@@ -11,6 +11,6 @@ def test_validate_config():
     validate_config(input)
 
 
-def test_get_job_name():
+def test_get_job_name(aws_credentials):
     job_name = get_job_name("foobar")
     assert job_name.startswith("ros-image-pipeline-foobar-")
