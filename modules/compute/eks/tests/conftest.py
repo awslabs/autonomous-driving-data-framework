a2b1de08a483487b20b831459525e148858dabc9
Gonzalo Barbeito <gonzalobarbeito@hotmail.com>
2023-12-12 18:01:25 +0100

    chore: delete modules that should use gitref

--- modules/compute/eks/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-import os
-
-
-def pytest_generate_tests(metafunc):
-    os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
-    os.environ["VERSIONS_DIR"] = "tests/versions"
