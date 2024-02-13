ae4aa05673872d7b423c1f327f32c0de34203b97
malachi-constant <lucascchanson@gmail.com>
2024-02-12 16:00:28 -0800

    formatting

--- modules/analysis/rosbag-image-pipeline-sfn/app.py
+++ modules/analysis/rosbag-image-pipeline-sfn/app.py
@@ -1,8 +1,8 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-import os
 import json
+import os
 
 from aws_cdk import App, CfnOutput, Environment
 
