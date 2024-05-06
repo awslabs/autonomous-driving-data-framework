f2e060f288e9f1e26e3645b4407db8da51a62449
malachi-constant <lucascchanson@gmail.com>
2024-05-06 16:02:23 -0700

    chore: add compatibility for manifest paths list

---
+++ modules/testing/integration-tests/artifacts/bin/create_manifest_list.py
@@ -0,0 +1,16 @@
+import sys
+from typing import List
+
+
+if len(sys.argv) > 1:
+    manifests: List[str] = list(sys.argv[1].strip('\"').split(','))
+else:
+    print("")
+    exit()
+
+output: str = ""
+for manifest in manifests:
+    manifest.strip(" ")
+    output += f'"{manifest}" '
+
+print(output)
