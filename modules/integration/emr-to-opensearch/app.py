7788cdda4b17e6e190e7d302cee155b331ef2ebd
Leon Luttenberger <luttenberger.leon@gmail.com>
2024-09-03 09:48:32 -0500

    minor refactor

--- modules/integration/emr-to-opensearch/app.py
+++ modules/integration/emr-to-opensearch/app.py
@@ -19,7 +19,8 @@ def _param(name: str) -> str:
 
 
 vpc_id = os.getenv(_param("VPC_ID"))  # required
-private_subnet_ids = json.loads(os.getenv("SEEDFARMER_PARAMETER_PRIVATE_SUBNET_IDS", ""))  # required
+private_subnet_ids = json.loads(os.getenv(_param("PRIVATE_SUBNET_IDS"), ""))  # required
+
 opensearch_sg_id = os.getenv(_param("OPENSEARCH_SG_ID"), "")
 opensearch_domain_name = os.getenv(_param("OPENSEARCH_DOMAIN_NAME"), "")
 opensearch_domain_endpoint = os.getenv(_param("OPENSEARCH_DOMAIN_ENDPOINT"), "")
