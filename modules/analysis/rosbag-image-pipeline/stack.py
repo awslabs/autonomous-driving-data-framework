1b0eb71d91283b18f543dace8c60069effbe78e8
kukushking <kukushkin.anton@gmail.com>
2024-12-20 15:08:59 +0000

    fix formatting

--- modules/analysis/rosbag-image-pipeline/stack.py
+++ modules/analysis/rosbag-image-pipeline/stack.py
@@ -121,7 +121,10 @@ class AwsBatchPipeline(Stack):
             iam.PolicyStatement(
                 actions=["s3:GetObject", "s3:GetObjectAcl", "s3:ListBucket"],
                 effect=iam.Effect.ALLOW,
-                resources=[f"arn:{self.partition}:s3:::{project_name}-*", f"arn:{self.partition}:s3:::{project_name}-*/*"],
+                resources=[
+                    f"arn:{self.partition}:s3:::{project_name}-*",
+                    f"arn:{self.partition}:s3:::{project_name}-*/*",
+                ],
             ),
         ]
         dag_document = iam.PolicyDocument(statements=policy_statements)
