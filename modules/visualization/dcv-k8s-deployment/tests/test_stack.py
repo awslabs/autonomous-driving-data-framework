2015057dc67deaf4454e825a41b6a01085037b4e
Xuezheng Ma <xmas@amazon.com>
2025-10-21 15:12:10 -0700

    chore: fix PR checks

--- modules/visualization/dcv-k8s-deployment/tests/test_stack.py
+++ modules/visualization/dcv-k8s-deployment/tests/test_stack.py
@@ -49,4 +49,4 @@ def test_app(stack_defaults):
     template = Template.from_stack(stack)
     template.resource_count_is("AWS::IAM::Role", 1)
     template.resource_count_is("AWS::IAM::Policy", 3)
-    template.resource_count_is("Custom::AWSCDK-EKS-KubernetesResource", 6)
+    template.resource_count_is("Custom::AWSCDK-EKS-KubernetesResource", 7)
